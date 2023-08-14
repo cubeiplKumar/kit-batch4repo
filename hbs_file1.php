dbfe99d677a7b9c56495e2bae456692bc2b0f863
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2023-08-14 13:22:20 +0530

    Update hbs_file1.php

--- hbs_file1.php
+++ hbs_file1.php
@@ -3,3 +3,7 @@ userid
 health_history_module
 registration
 status
+
+#New Feature on WeatherReport Module included
+-Connect to the Weather Report Official site
+-Fetch the latest env. changes and update in the portal
