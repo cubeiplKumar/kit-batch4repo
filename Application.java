55118aad0bb671a6d8d5778101c060214f82171d
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2023-09-01 13:12:30 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -46,18 +46,6 @@ public class ReadFromConsole {
 
 		#feature201 changes done by JOHN on reports module
 		public class MyExceptionHandle {
-    public static void main(String a[]){
-        try{
-            for(int i=5;i>=0;i--){
-                System.out.println(10/i);
-            }
-        } catch(Exception ex){
-            System.out.println("Exception Message: "+ex.getMessage());
-            ex.printStackTrace();
-        }
-        System.out.println("After for loop...");
-
-    }
-}
+    
     }
 }
